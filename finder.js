3f4cedd6aa8da6e69ff8d15856b7796e4d4efbc9
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-07-04 13:08:19 +0300

    The first commit with beginning files

---
+++ finder.js
@@ -0,0 +1,55 @@
+'use strict';
+const fs = require('fs');
+const Client = require('node-rest-client').Client;
+const client = new Client();
+const fileDB = './resources/R&MDB.json'
+const startUrl = "https://rickandmortyapi.com/api/character/";
+
+async function main(parameters) {
+    fs.writeFileSync(fileDB, '{\"results\":[]}');
+    getBase(startUrl).then((resolve) => {
+        setTimeout(() => {
+            const character = getCharacter(parameters);
+            console.log(character);
+            fs.writeFileSync('./resources/result.json', JSON.stringify(character), 'utf8');
+        }, 10000);
+    })
+        .catch((err) => { console.log(err); })
+};
+
+function getBase(url) {
+    return new Promise((resolve, reject) => {
+        try {
+            client.get(url, (data, response) => {
+                if (response.statusCode === 200) {
+                    let array = JSON.parse(fs.readFileSync(fileDB, 'utf8')).results;
+                    array = [...array, ...data.results];
+                    fs.writeFileSync(fileDB, '{\"results\": ' + JSON.stringify(array) + '}', 'utf-8');
+                    url = data.info.next;
+                    url === '' ? console.log('Data recieved!') : getBase(url);
+                }
+                else {
+                    console.log('Something went wrong! Response code:' + response.statusCode);
+                }
+            });
+            return resolve('success');
+        } catch (error) {
+            return reject(error);
+        }
+    });
+}
+
+function getCharacter(parameters) {
+    let dataBase = JSON.parse(fs.readFileSync(fileDB, 'utf8')).results;
+    Object.keys(parameters).forEach(elem => {
+        if (elem === 'id') {
+            dataBase = dataBase.filter(
+                element => element[elem].toString().toLowerCase() === parameters[elem].toString().toLowerCase());
+        } else {
+            dataBase = dataBase.filter(
+                element => element[elem].toString().toLowerCase().indexOf(parameters[elem].toString().toLowerCase()) > -1);
+        }
+    });
+    return dataBase;
+}
+exports.main = main;
---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+const finder = require('./finder.js');
+const args = require('yargs')
+    .usage('Usage: node $0 --id [string] --type [string]')
+    .example('$0 -n Alex').example('$0 --st Alive')
+    .option('id').alias('i', 'id').nargs('id', 1).describe('i', 'Number of ID')
+    .option('name').alias('n', 'name').nargs('n', 1).describe('n', 'Name of character')
+    .option('status').alias('st', 'status').nargs('st', 1).describe('st', 'Status: alive or dead')
+    .option('species').alias('sp', 'species').nargs('sp', 1).describe('sp', 'Species of character(e.g. Human)')
+    .option('type').alias('t', 'type').nargs('t', 1).describe('t', 'Type')
+    .option('gender').alias('g', 'gender').nargs('g', 1).describe('g', 'Gender: male or female')
+    .option('origin').alias('o', 'origin').nargs('o', 1).describe('o', 'Origin(e.g. Earth)')
+    .option('location').alias('l', 'location').nargs('l', 1).describe('l', 'Location(e.g. Earth)')
+    .help('h').alias('h', 'help')
+    .epilogue('Created by Dzmitry Karneyenka')
+    .argv
+
+function second() {
+    const keys = ['id', 'name', 'species', 'status', 'type', 'gender', 'origin', 'location'];
+    let result = '{';
+    keys.forEach((el) => {
+        result += typeof (args[el]) === 'undefined' ? '' : '\"' + el + '\":\"' + args[el] + '\",';
+    });
+    return JSON.parse(result.slice(0, -1) + '}');
+}
+
+finder.main(second());
+
+
+
+
