9345fcb2c58bff9ea030d1b7190a2951374e76d0
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-07-04 13:29:20 +0300

    working version of finder character app

--- finder.js
+++ finder.js
@@ -6,33 +6,33 @@ const fileDB = './resources/R&MDB.json'
 const startUrl = "https://rickandmortyapi.com/api/character/";
 
 async function main(parameters) {
-    fs.writeFileSync(fileDB, '{\"results\":[]}');
-    getBase(startUrl).then((resolve) => {
-        setTimeout(() => {
-            const character = getCharacter(parameters);
+    getBase(startUrl)
+        .then(() => getCharacter(parameters))
+        .then(character => {
             console.log(character);
             fs.writeFileSync('./resources/result.json', JSON.stringify(character), 'utf8');
-        }, 10000);
-    })
-        .catch((err) => { console.log(err); })
+        })
+        .catch(error=>console.log(error));
 };
 
 function getBase(url) {
+    let array = [];
+    fs.existsSync(fileDB) ? '' : fs.writeFileSync(fileDB, '{"results":[]}', 'utf8');
     return new Promise((resolve, reject) => {
         try {
             client.get(url, (data, response) => {
                 if (response.statusCode === 200) {
-                    let array = JSON.parse(fs.readFileSync(fileDB, 'utf8')).results;
+                    array = JSON.parse(fs.readFileSync(fileDB, 'utf8')).results;
                     array = [...array, ...data.results];
                     fs.writeFileSync(fileDB, '{\"results\": ' + JSON.stringify(array) + '}', 'utf-8');
                     url = data.info.next;
-                    url === '' ? console.log('Data recieved!') : getBase(url);
+                    return url === '' ? array : getBase(url);
                 }
                 else {
                     console.log('Something went wrong! Response code:' + response.statusCode);
                 }
             });
-            return resolve('success');
+            return resolve(array);
         } catch (error) {
             return reject(error);
         }
